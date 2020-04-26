b16f80ea4b6e704c6d5f2e385a0983fdc476f903
marcszeg <marcszeg27@gmail.com>
2020-04-26 13:29:36 +0200

    BookGenerator.java and BookDao.java created

--- src/main/java/book/BookDao.java
+++ src/main/java/book/BookDao.java
@@ -1,4 +1,21 @@
 package book;
 
-public class BookDao {
+import book.model.Book;
+import jpa.GenericJpaDao;
+import user.model.User;
+
+import javax.persistence.NoResultException;
+import java.util.Optional;
+
+public class BookDao extends GenericJpaDao<User> {
+
+    public Optional<Book> findByIsbn13(String isbn13) {
+        try{
+            return Optional.of(entityManager.createQuery("SELECT b FROM Book b WHERE b.isbn13 = :isbn13", Book.class)
+                    .setParameter("isbn13",isbn13)
+                    .getSingleResult());
+        } catch (NoResultException e) {
+            return Optional.empty();
+        }
+    }
 }
---
+++ src/main/java/book/BookGenerator.java
@@ -0,0 +1,32 @@
+package book;
+
+import book.model.Book;
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class BookGenerator {
+    private static Faker faker = new Faker(new Locale("en"));
+
+    private static Book createBook() {
+        Date date = faker.date().past(100000, TimeUnit.DAYS);
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+
+        Book book = Book.builder()
+                .isbn13(faker.code().isbn13())
+                .author(faker.book().author())
+                .title(faker.book().title())
+                .format(faker.options().option(Book.Format.class))
+                .publisher(faker.book().publisher())
+                .publicationDate(localDate)
+                .pages(faker.number().numberBetween(10,1000))
+                .avaible(faker.bool().bool())
+                .build();
+
+        return book;
+    }
+}
